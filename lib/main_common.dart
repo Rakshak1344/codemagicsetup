1166ce4a6f0c26b258519d38b51350f5587cfa6a
Rakshak1344 <rakshithgajendra@gmailc.om>
2020-03-04 22:39:11 +0530

    Initial commit

---
+++ lib/main_common.dart
@@ -0,0 +1,29 @@
+import 'package:codemagicsetup/app_widget.dart';
+import 'package:codemagicsetup/config_reader.dart';
+import 'package:codemagicsetup/environment.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+Future<void> mainCommon(String env) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ConfigReader.initialize();
+
+  Color primaryColor;
+
+  switch (env) {
+    case Environment.dev:
+      primaryColor = Colors.blue;
+      break;
+    case Environment.prod:
+      primaryColor = Colors.red;
+      break;
+  }
+
+  runApp(
+    Provider.value(
+      value: primaryColor,
+      child: MyApp(),
+    ),
+  );
+}
---
+++ lib/main_dev.dart
@@ -0,0 +1,6 @@
+import 'package:codemagicsetup/environment.dart';
+import 'package:codemagicsetup/main_common.dart';
+
+Future<void> main() async {
+  await mainCommon(Environment.dev);
+}
